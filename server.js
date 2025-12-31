28a1e546599792feb6abf31e16fea34ed048f851
Jashan-20 <jashanjeetsingh5050@gmail.com>
2025-12-31 14:27:31 +0530

    Fix server to use environment variables

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json()); 
 
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
